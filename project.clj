401dcd34018a2bca5d4d0bdf8712356af40e384e
Christian Egli <christian@egli.cc>
2021-04-08 16:30:21 +0200

    Update deps

--- project.clj
+++ project.clj
@@ -3,12 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.apache.chemistry.opencmis/chemistry-opencmis-client-impl "1.1.0"]
                  [sanitize-filename "0.1.0"]
                  [org.clojure/data.xml "0.2.0-alpha5"]
-                 [org.clojure/data.zip "0.1.2"]
-                 [org.clojure/data.csv "0.1.4"]]
+                 [org.clojure/data.zip "1.0.0"]
+                 [org.clojure/data.csv "1.0.0"]]
   :main ^:skip-aot cmis-clj.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
